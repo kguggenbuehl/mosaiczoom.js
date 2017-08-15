eafe775af19c932cefe604ad9eb69b8e712905a0
Kaspar Guggenbühl <k.guggenbuehl@gmail.com>
2017-08-15 14:25:09 +0200

    new structure

---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+/* global require */
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var browserSync = require('browser-sync').create();
+var autoprefixer = require('gulp-autoprefixer');
+var path = require('path');
+
+/* Change your directory and settings here */
+var settings = {
+    publicDir: '.',
+    sassDir: 'css',
+    cssDir: 'css'
+};
+
+/**
+ * serve task, will launch browserSync and launch index.html files,
+ * and watch the changes for html and sass files.
+ **/
+gulp.task('serve', ['sass'], function() {
+
+    /**
+     * Launch BrowserSync from publicDir
+     */
+    browserSync.init({
+        server: settings.publicDir,
+        browser: "google chrome"
+    });
+
+    /**
+     * watch for changes in sass files
+     */
+    gulp.watch([settings.sassDir + "/**/*.scss", settings.sassDir + "/*.scss"], ['sass']);
+
+    /**
+     * watch for changes in html and php files
+     */
+    gulp.watch([settings.publicDir + "/*.html", settings.publicDir + "/*.php"]).on('change', browserSync.reload);
+
+});
+
+/**
+ * sass task, will compile the .SCSS files,
+ * and handle the error through plumber and notify through system message.
+ */
+gulp.task('sass', function() {
+    return gulp.src(settings.sassDir + "/*.scss")
+        .pipe(sourcemaps.init())
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(autoprefixer('last 2 version'))
+        .pipe(gulp.dest(settings.cssDir))
+        .pipe(browserSync.stream());
+});
+
+/**
+ * Default task, running just `gulp` will compile the sass,
+ * compile the site, launch BrowserSync then watch
+ * files for changes
+ */
+gulp.task('default', ['serve']);
